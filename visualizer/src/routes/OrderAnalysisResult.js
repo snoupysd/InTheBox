1d318a740090959021ba3ea272751ec677897694
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-06-06 12:38:26 +0200

    Write minimal visualisation of order analysis (ugly)

--- visualizer/src/routes/OrderAnalysisResult.js
+++ visualizer/src/routes/OrderAnalysisResult.js
@@ -1,21 +1,47 @@
 import { Box } from "@mui/system";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectResponse } from "../store/orderApiSlice/orderApiSlice";
+
+
 import { Item } from "../helper/styleHelper";
 import { Typography, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 
 export function OrderAnalysisResult() {
-    const dispatch = useDispatch();
+    const response = useSelector(selectResponse);
+    const data = response.data
     const navigate = useNavigate();
 
+    if (!data || !data.binRecommandations || data.binRecommandations.length === 0) {
+        return <div>No data available.</div>;
+    }
+
     return (<>
-        <div style={{ display: 'grid', gridTemplateColumns: '10% 10% 10%', columnGap: "35%" }}>
-            <h1>Order Analysis Result</h1>
-            <Button variant="contained" onClick={() => navigate("/")}>Back Home</Button>
+        <Typography sx={{ fontSize: "2rem", fontWeight: "700" }}>Order Analysis Result</Typography>
+        <div>
+            {data.binRecommandations.map((recommendation, index) => (
+                <div key={index}>
+                    {recommendation && (
+                            <>
+                            <h2>Using {index} different sizes</h2>
+                            <ul>
+                                {recommendation.bins.map((bin, binIndex) => (
+                                    <li key={binIndex}>
+                                        <h3>Bin {binIndex}</h3>
+                                        <p>x: {bin.x}</p>
+                                        <p>y: {bin.y}</p>
+                                        <p>z: {bin.z}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                            </>
+                    )}
 
+                </div>
+
+            ))}
         </div>
-
+        <Button variant="contained" onClick={() => navigate("/")}>Back Home</Button>
     </>);
 }
